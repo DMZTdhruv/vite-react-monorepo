15de16d38fadf84b45a38a1ac5b1115701a2ebbb
DMZTdhruv <shruvparmar@gmail.com>
2024-11-24 18:04:32 +0530

    first commit

---
+++ apps/dashboard/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/components/button";
+
+function App() {
+  return (
+    <main className="h-screen w-full bg-black text-white">
+      <Button>Hello world</Button>
+    </main>
+  );
+}
+
+export default App;
